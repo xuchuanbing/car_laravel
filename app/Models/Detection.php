1a9bb35002df2066172b10d96543c168f062c4e5
pengtaom <pengtaopt01@163.com>
2017-07-03 17:43:52 +0800

    m

---
+++ app/Models/Detection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Detection extends Model
+{
+    protected $table = "detection";
+    public $timestamps = false;
+}
